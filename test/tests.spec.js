49fa583f94e414d30adbd5f4eaec2a61ad855003
Dario Pedicini <d.pedicini@gmail.com>
2017-06-12 02:47:00 +0200

    Add tests and skeleton service

---
+++ test/tests.spec.js
@@ -0,0 +1,61 @@
+var should = require('should');
+var io = require('socket.io-client');
+
+var listenerService = 'http://0.0.0.0:3000';
+var broadcastingService = 'http://0.0.0.0:3001';
+
+var options = {
+	transports: ['websocket'],
+	'force new connection': true
+};
+
+var message = {text: 'Hello World!'};
+var messageSender;
+var subscriber;
+
+describe('The first service', function(){
+	it('Should listen for incoming messages through the websocket protocol', 
+		function(done){
+		
+		messageSender = io.connect(listenerService, options);
+		messageSender.on('connect', function(){
+			done();
+		});
+	});
+
+
+});	
+
+describe('The second service', function(){
+	it ("Should publish received messages to subscribers", function(done){
+		messageSender = io.connect(listenerService, options);
+		subscribers = io.connect(broadcastingService, options);
+
+		messageSender.on('connect', function(){
+		
+			messageSender.emit('message', message);
+			
+			subscribers.on('message', function(msg){
+				message.should.equal(msg);
+				done();
+			});	
+		});
+	});
+});
+/*
+	it('Should publish received messages into a message queue', function(done){
+		messageSender =  io.connect(listenerService, options);
+		
+		messageSender.on('connect', function(){
+			messageSender.on('message', function(msg){
+				messageSender.on('message_received', function(msg){
+
+				});
+			});
+			messageSender.emit(message);
+			done();
+		});
+
+	});
+
+*/
